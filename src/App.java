cde3ec40761aedb87b64594cb5f351bc2d359828
unknown <harun>
2024-03-11 08:20:48 +0400

    test run with print statement: great success!!

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello world i am the create chat application in the java of the programming language waaaoo");
     }
 }
