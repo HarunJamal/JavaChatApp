62c9600f6022752074553e938bb45f2f78f66bd7
unknown <harun>
2024-03-13 06:19:24 +0400

    creating server to accept client chat request

---
+++ src/ChatServer.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+    public static void main(String[] args) throws IOException{
+        ServerSocket servsoc = new ServerSocket(6969);
+    }
+}
